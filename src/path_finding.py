03996fb648d8c79eb1c8881fbdc3eb7f97454fed
Maria Navarro <mariadvnr04@gmail.com>
2026-06-23 12:54:51 -0400

    x

--- src/path_finding.py
+++ src/path_finding.py
@@ -1,7 +1,8 @@
 import math
 from src.config import CELL_SIZE
 
-def heuristic(cell_a: Cell, cell_b: Cell):
+
+def heuristic(cell_a, cell_b):
     a = abs(cell_b.x - cell_a.x) * CELL_SIZE
     b = abs(cell_b.y - cell_a.y) * CELL_SIZE
 
